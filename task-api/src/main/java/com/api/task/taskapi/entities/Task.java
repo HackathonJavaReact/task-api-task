7352906ad8b7f31ec4e5268e5bb5a42b745eab22
cjanie <janiechun@hotmail.fr>
2021-05-01 17:14:13 +0200

    finalized

--- task-api/src/main/java/com/api/task/taskapi/entities/Task.java
+++ task-api/src/main/java/com/api/task/taskapi/entities/Task.java
@@ -2,7 +2,6 @@ package com.api.task.taskapi.entities;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
